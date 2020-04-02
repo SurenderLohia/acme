9b4fbd286fa1da93f0e29a54d76a8d8f329ecabf
Surender Lohia <surender.lohia.e@gmail.com>
2020-04-03 00:00:59 +0530

    Added full text search

--- src/App.js
+++ src/App.js
@@ -13,14 +13,26 @@ function App() {
   const [hasNextPage, setHasNextPage] = useState([]);
   const [isNextPageLoading, setIsNextPageLoading] = useState([]);
   
-  async function fetchData({page = 1, limit = 12} = {}) {
-    const query = `?_page=${page}&_limit=${limit}`;
+  async function fetchData({page = 1, limit = 12, isSearchQuery = false, searchText = ''} = {}) {
+    let query = `?_page=${page}&_limit=${limit}`;
+    if(isSearchQuery) {
+      query += `&q=${searchText}`;
+    } 
+    
     const result = await fetch((PHOTOS_URL + query));
     const json = await result.json();
 
     setHasNextPage(rows.length < TOTAL_ROWS);
     setIsNextPageLoading(false);
-    setRows([...rows, ...json]);
+
+    // If: First it's time search then replase rows with search result rows
+    if(isSearchQuery && page === 1) {
+      console.log('comes here');
+      setRows(json);
+    } else {
+      // else append result with exsisting rows
+      setRows([...rows, ...json]);
+    }
   }
 
   const _loadNextPage = (query) => {
--- src/components/dataTable/DataTable.js
+++ src/components/dataTable/DataTable.js
@@ -7,7 +7,7 @@ import './data-table.css';
 import DataTableHeaderRow from './DataTableHeaderRow';
 import DataTableBodyRow from './DataTableBodyRow';
 
-const getRowsInitialState = function(rows) {
+const getRowsInitialState = function(rows = []) {
   const rowsInitialState = {};
   rows.forEach(row => {
     const rowId = row.id;
@@ -21,6 +21,7 @@ const getRowsInitialState = function(rows) {
 function DataTable(props) {
   const [ rowsState,  setRowsState ] = useState(getRowsInitialState(props.rows));
   const [ page, setPage ] = useState(props.page);
+  const [ searchText, setSearchText ] = useState('');
 
   const {
     rows,
@@ -84,54 +85,97 @@ function DataTable(props) {
 
   // If there are more items to be loaded then add an extra row to hold a loading indicator.
   const itemCount = hasNextPage ? rows.length + 1 : rows.length;
+  console.log('itemCount', itemCount);
 
   // Only load 1 page of items at a time.
   // Pass an empty callback to InfiniteLoader in case it asks us to load more than once.
+  console.log('isNextPageLoading', isNextPageLoading);
   const loadMoreItems = isNextPageLoading ? () => {} : loadNextPage;
 
   // Every row is loaded except for our loading indicator row.
   const isItemLoaded = index => !hasNextPage || index < rows.length;
+  //console.log('isItemLoaded', isItemLoaded(index));
 
   const onLoadMoreItems = (query) => {
     if(!isNextPageLoading) {
-      setPage(query.page);
+      if(!query.isSearchQuery) {
+        query.page += 1;
+        setPage(query.page);
+      }
     }
     
+    console.log('page', query.page);
     loadMoreItems(query);
   }
 
+  const onSearchTextChange = (e) => {
+    setSearchText(e.target.value);
+  }
+
+  const onSearchSubmit = (e) => {
+    e.preventDefault();
+    const serachQuery = {
+      isSearchQuery: true,
+      searchText,
+      page: 1
+    }
+    
+    setPage(2);
+    onLoadMoreItems(serachQuery);
+  }
+
   return(
-    <div className="Rtable">
-      <DataTableHeaderRow
-        columns={props.columns}
-        handleSelectAllChange={handleSelectAllChange}
-      />
-      <InfiniteLoader
-        isItemLoaded={isItemLoaded}
-        itemCount={itemCount}
-        loadMoreItems={() => onLoadMoreItems({page: page + 1})}
-      >
-        {({ onItemsRendered, ref }) => (
-        <List
-          itemData={{
-            rows,
-            columns,  
-            onRowClick,
-            rowsState,
-            handleRowCheckboxChange,
-            isItemLoaded
-          }}
-          className="List"
-          height={window.innerHeight - tableHeaderHeight}
+    <div className="data-table">
+      <form className="search-filter-container flex-row" onSubmit={onSearchSubmit}>
+        <div>
+          <select className="search-filter-type">
+            <option value="">Filter</option>
+            {columns.map((column) => {
+              return <option value={column.id} key={column.id}>{column.label}</option>
+            })}
+          </select>
+        </div>
+        <div className="flex-item">
+          <input 
+            className="search-filter-input" 
+            type="text" 
+            placeholder="Search photos"
+            onChange={onSearchTextChange}
+          />
+        </div>
+      </form>
+      <div className="Rtable">
+        <DataTableHeaderRow
+          columns={props.columns}
+          handleSelectAllChange={handleSelectAllChange}
+        />
+        <InfiniteLoader
+          isItemLoaded={isItemLoaded}
           itemCount={itemCount}
-          itemSize={80}
-          onItemsRendered={onItemsRendered}
-          ref={ref}
+          loadMoreItems={() => onLoadMoreItems({page: page})}
         >
-         {DataTableBodyRow}
-        </List>
-        )}
-      </InfiniteLoader>
+          {({ onItemsRendered, ref }) => (
+          <List
+            itemData={{
+              rows,
+              columns,  
+              onRowClick,
+              rowsState,
+              handleRowCheckboxChange,
+              isItemLoaded
+            }}
+            className="List"
+            height={window.innerHeight - tableHeaderHeight}
+            itemCount={itemCount}
+            itemSize={80}
+            onItemsRendered={onItemsRendered}
+            ref={ref}
+          >
+          {DataTableBodyRow}
+          </List>
+          )}
+        </InfiniteLoader>
+      </div>
     </div>
   )
 };
