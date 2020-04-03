fa94a36ddd53d951ea26c59b5010a6ec0867b170
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-03 15:28:00 +0000

    Deploy SurenderLohia/acme to github.com/SurenderLohia/acme.git:gh-pages

---
+++ precache-manifest.0c4bc413fa9f46f00f4aea870ac65fde.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f385eeef65b4802cc743968655ef1d82",
+    "url": "/acme/index.html"
+  },
+  {
+    "revision": "ae3c33d32d061f554ed6",
+    "url": "/acme/static/css/main.7953cfea.chunk.css"
+  },
+  {
+    "revision": "1d7f350836eb2c97dec6",
+    "url": "/acme/static/js/2.6d2e6803.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/acme/static/js/2.6d2e6803.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae3c33d32d061f554ed6",
+    "url": "/acme/static/js/main.cef65167.chunk.js"
+  },
+  {
+    "revision": "494fdd4a7503189afe85",
+    "url": "/acme/static/js/runtime-main.f79ce407.js"
+  }
+]);
